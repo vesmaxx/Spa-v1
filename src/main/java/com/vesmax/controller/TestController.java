3f40fb62f4000d5ef089acac0cc5bb8c5e027dc5
Nam PH <namph@techfox.io>
2019-07-03 21:56:56 +0700

    Namph-03/07

--- src/main/java/com/vesmax/controller/TestController.java
+++ src/main/java/com/vesmax/controller/TestController.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-	@RequestMapping(value="abc")
+	@RequestMapping(value="client")
 	public String abc() {
-		System.out.println("hhhhh");
+		System.out.println("client");
 		return "client/abc";
 	}
 	
-	@RequestMapping(value="cde")
+	@RequestMapping(value="admin")
 	public String cde() {
-		System.out.println("hhhhh");
+		System.out.println("admin");
 		return "admin/cde";
 	}
 
