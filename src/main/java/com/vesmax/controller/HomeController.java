96726ee8bab53a25484aa9d1031be1acaa96e82e
Nam PH <namph@techfox.io>
2019-08-05 22:54:43 +0700

    login-signout

--- src/main/java/com/vesmax/controller/HomeController.java
+++ src/main/java/com/vesmax/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.vesmax.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -9,10 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	@RequestMapping(value = "/")
-	public String abc(Model model) {
+	public String abc(Model model, HttpSession session) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String email = auth.getName(); // get logged in username
-		System.out.println(email + "this is email");
+//		String email = auth.getName(); // get logged in username
+//		System.out.println(email + "this is email");
+		model.addAttribute("Email", auth.getName());
+//		session.setAttribute("Email", auth.getName());
 		System.out.println("client");
 		return "client/homePage";
 	}
